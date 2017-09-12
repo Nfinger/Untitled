188a37087b682a042a6f8d9a5f08d71d84cca4ba
Kevin Rondo <krondo@filtrine.com>
2017-09-11 23:00:31 -0400

    refactor env and test script

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 // Set env variable for correct db connection; package.json sets the test env
 var env = process.env.NODE_ENV || 'development';
-console.log('env**********',env);
 if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/HeadStart'
