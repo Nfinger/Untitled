f0ee931bf564a6c7183f9265ec5bbf0ae62c5183
Kevin Rondo <krondo@filtrine.com>
2017-09-28 21:55:06 -0400

    User login route and auth

--- server.js
+++ server.js
@@ -89,6 +89,20 @@ app.get('/user/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+// Login route
+app.post('/user/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  // Try and validate provided user credentials
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      // Set new x-auth token and return the user
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
